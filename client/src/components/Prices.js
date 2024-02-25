4d4ce18f2572aefa2749e62c88955398f298db48
4ASIM <masim4872@gmail.com>
2024-02-25 11:27:11 +0500

    Initial commit

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "$2000 to 4900",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "$5000 to 12000",
+    array: [20, 39],
+  },
+  {
+    _id: 2,
+    name: "$13000 to 20000",
+    array: [40, 59],
+  },
+  {
+    _id: 3,
+    name: "$21000 to 30000",
+    array: [60, 79],
+  },
+  {
+    _id: 4,
+    name: "$31000 to 32000",
+    array: [80, 99],
+  },
+  {
+    _id: 4,
+    name: "32000 or more",
+    array: [100, 9999],
+  },
+];
